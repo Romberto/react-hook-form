73978da2813d07bd3e62c1d495433a91f45b2201
Romberto <korhoff@mail.ru>
2025-03-25 15:29:30 +0400

    add: add errorBoundary class

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,7 +8,7 @@ export const Main: React.FC = () => {
   if (isError) return <p>что то пошло не так ;\</p>;
 
   return (
-    <ul className="grid grid-cols-1 grig-rows-auto items-start justify-center gap-4 pt-20 md:grid-cols-2">
+    <ul className="grid grid-cols-1 grig-rows-auto items-start justify-center gap-4 pt-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data?.map((item) => {
         return <Item key={item.id} item={item} />;
       })}
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import { store } from './app/store.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
+import ErrorBoundary from "./components/UI/ErrorBoundary/ErrorBoundary.tsx";
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    
-  </StrictMode>,
-)
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+  </StrictMode>
+);
